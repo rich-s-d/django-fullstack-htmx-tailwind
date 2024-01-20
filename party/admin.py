39cd53a63b691e776a6fa7e8bae06a84e177e7b6
Shane Rich <shanerich@Shanes-MacBook-Pro.local>
2024-01-20 15:49:27 +0100

    Ran migrations for new models, and registered models in admin.py.

--- party/admin.py
+++ party/admin.py
@@ -1,3 +1,26 @@
-from django.contrib import admin
+# party/admin.py
 
-# Register your models here.
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+
+from .models import CustomUser, Party, Gift, Guest
+
+
+@admin.register(CustomUser)
+class UserAdmin(UserAdmin):
+    pass
+
+
+@admin.register(Party)
+class PartyAdmin(admin.ModelAdmin):
+    readonly_fields = ("uuid",)
+
+
+@admin.register(Gift)
+class GiftAdmin(admin.ModelAdmin):
+    readonly_fields = ("uuid",)
+
+
+@admin.register(Guest)
+class GuestAdmin(admin.ModelAdmin):
+    readonly_fields = ("uuid",)
